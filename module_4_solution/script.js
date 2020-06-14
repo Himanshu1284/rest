22f54f6e7dcc14575059c8b576c7fcbae136058f
Himanshu <himg1512@gmail.com>
2020-06-14 19:41:18 +0530

    Update

---
+++ module_4_solution/script.js
@@ -0,0 +1,42 @@
+// ******************************* 
+// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
+// Module 4 Assignment.
+// on the console should look like this:
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+*/
+
+(function() {
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+    for (var i = 0; i < names.length; i++) {
+
+        var firstLetter = names[i].charAt(0).toLowerCase();
+
+        if (firstLetter === 'j') {
+
+            byeSpeaker.speak(names[i]);
+
+        } 
+
+        else {
+
+            helloSpeaker.speak(names[i]);
+
+        }
+
+    }
+    
+})();
